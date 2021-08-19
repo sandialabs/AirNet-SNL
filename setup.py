d55081359df3b89eab4f7ebcc1c532883428f156
dlee1 <dennis.lee.8942@gmail.com>
2021-08-19 17:01:05 -0600

    Update setup

--- setup.py
+++ setup.py
@@ -18,19 +18,23 @@ convolutional neural networks for sparse data.
 
 Documentation:
 
+Dennis J. Lee. (2021). sandialabs/AirNet-SNL:
+First release of AirNet-SNL (v1.0.0). Zenodo.
+https://doi.org/10.5281/zenodo.5192883
+
 D. J. Lee, J. Mulcahy-Stanislawczyk, E. Jimenez, D. West,
 R. Goodner, C. Epstein, K. Thompson, and A. L. Dagel,
 "AirNet-SNL: End-to-End Training of Iterative Reconstruction
 and Deep Neural Network Regularization for Sparse-Data XPCI CT,"
  OSA Imaging and Applied Optics Congress, 2021.
 
-Source code: https://github.com/dennis-j-lee/asnl
+Source code: https://github.com/sandialabs/AirNet-SNL
 """
 
 DISTNAME = 'AirNet-SNL'
 MAINTAINER = "Dennis Lee"
 LICENSE = 'Revised BSD'
-URL = 'https://github.com/dennis-j-lee/asnl'
+URL = 'https://github.com/sandialabs/AirNet-SNL'
 
 TESTS_REQUIRE = [
     'pytest',
